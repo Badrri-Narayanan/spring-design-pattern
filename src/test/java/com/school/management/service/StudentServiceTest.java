07f08b29cf6c42e225d00d73a98a3b050e751b62
Badrri Narayanan S <badrri1995@gmail.com>
2022-09-06 10:49:11 +0530

    made change to use annotation in Mockito Junit

--- src/test/java/com/school/management/service/StudentServiceTest.java
+++ src/test/java/com/school/management/service/StudentServiceTest.java
@@ -1,29 +1,33 @@
 package com.school.management.service;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.school.management.model.Student;
 import com.school.management.model.Students;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
 
+	@Mock
     PeopleService studentService;
     
     @BeforeEach
     void setup() {
-    	studentService = mock(PeopleService.class);
     	List<Student> mockStudents = new ArrayList<Student>();
     	mockStudents.add(new Student());
+    	
     	Mockito.when(studentService.getAllEntities("")).thenReturn(new Students(mockStudents));
     }
 
--- src/test/java/com/school/management/service/TeacherServiceTest.java
+++ src/test/java/com/school/management/service/TeacherServiceTest.java
@@ -1,25 +1,30 @@
 package com.school.management.service;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.school.management.model.Teacher;
 import com.school.management.model.Teachers;
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class TeacherServiceTest {
 
+	@Mock
 	PeopleService teacherService;
 	
 	@BeforeEach
 	void setup() {
-		teacherService = mock(PeopleService.class);
 		List<Teacher> mockTeachers = new ArrayList<>();
 		mockTeachers.add(new Teacher());
 		
